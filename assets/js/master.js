afa1f120b8d303726e373dd7a35040f707149a40
McCollinsMediaWeb <111267011+McCollinsMediaWeb@users.noreply.github.com>
2025-03-28 14:51:36 +0530

    ssss

--- assets/js/master.js
+++ assets/js/master.js
@@ -73,3 +73,39 @@ function updateBodyClass() {
 // Add event listeners
 window.addEventListener("wheel", smoothScroll);
 window.addEventListener("scroll", updateBodyClass);
+
+
+$(document).ready(function() {
+    $('.brands').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
