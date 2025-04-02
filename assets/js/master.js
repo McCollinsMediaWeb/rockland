98976b7bb4cd7b12946a1369052c1c7aa560deb0
McCollinsMediaWeb <111267011+McCollinsMediaWeb@users.noreply.github.com>
2025-04-02 13:50:12 +0530

    cccc

--- assets/js/master.js
+++ assets/js/master.js
@@ -132,4 +132,18 @@ $(document).ready(function() {
     $(".ActivateMenu").click(function() {
         $("body").toggleClass("popupactivated");
     });
+    if (jQuery(window).width() < 1000) {
+        jQuery(".ImageSwitcher")
+        .fadeOut(400, function() {
+          jQuery(this).attr('src',jQuery(this).attr('data-mobile'));
+        })
+        .fadeIn(400);
+    }
+    else {
+      jQuery(".ImageSwitcher")
+        .fadeOut(400, function() {
+          jQuery(this).attr('src',jQuery(this).attr('data-desktop'));
+        })
+        .fadeIn(400);
+    }
 });
